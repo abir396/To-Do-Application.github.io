1f83b271d6436094809703656747f217dc38acbe
ABIR PAL <95144374+abir396@users.noreply.github.com>
2024-07-04 12:34:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,62 +1,51 @@
-var addLoopCount=0;
-var slLoopCount =0;
-
-//BlankCheck for Text
-function blankcheck(){
-    var x = document.forms["new-task-form"]["task"].value;
-    //alert (x);
-    if (x == "") {
-        alert("Task must be filled out");
-        return false;
-     }
-}
-// Preview task and Description
-function previewForm(){
-    addLoopCount=addLoopCount+1;
-    slLoopCount=slLoopCount+1;
-
-    var table = document.getElementById("tableContainer");
-
-    var row = table.insertRow(addLoopCount); // for sl.no.
-    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-    var cell1 = row.insertCell(0);   //for sl no 
-    var cell2 = row.insertCell(1);   //for task title 
-    var cell3 = row.insertCell(2);   //for task description 
-    var cell4 = row.insertCell(3);   //for completed 
-    var cell5 = row.insertCell(4);   //for delete
-
-    // Add some text to the new cells:
-    cell1.innerHTML = slLoopCount;
-    cell2.innerHTML = document.forms["new-task-form"]["task"].value;
-    cell3.innerHTML = document.forms["new-task-form"]["description"].value;
-    cell4.innerHTML = "<button onclick='Completed("+addLoopCount+"); return false;'>Completed</button>";
-    cell5.innerHTML = "<button onclick='Delete("+addLoopCount+"); return false;'>Delete</button>";
-
-    return false;
-
-}
-//comlete button function
-function Completed(count){
-    alert("Task no : "+count+" is completed");
-    var x = document.getElementById("tableContainer").getElementsByTagName("tr");
-    x[count].style.backgroundColor = "green";   
-    return false;
-   
-}
-// Delete button function
-function Delete(count){
-    alert("Are you sure to delete task no : "+count+" ?");
-	document.getElementById("tableContainer").deleteRow(count);
-    addLoopCount=addLoopCount-1;
-    return false;
+//for map
+let mapOptions = {
+    center:[20.5937, 78.9629],
+    zoom:5
 }
 
-//For validate  blank check and peview 
-function validateForm(){
-    blankcheck();
-    previewForm();
-    return false;
-   
-}
+let map = new L.map('map' , mapOptions);
 
+let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+map.addLayer(layer);
+
+//API key for map
+let apiKey = "9098219d18994560be55415be86df062",
+    marker = null;
+
+
+const addressSearchControl = L.control.addressSearch(apiKey, {
+    position: 'topleft',
+
+	//set it true to search addresses nearby first
+    mapViewBias:true,
+
+    //for Address Search field when it's empty
+    placeholder:"Enter an address here",
+
+    //callback to notify when a user has selected an address
+    resultCallback: (address) => {
+		// If there is already a marker remove it
+        if (marker) {
+          	marker.remove();
+        }
+		//for showing rrors when the address search box is empty
+		if (!address) {
+				return;
+		}
+     	
+		//add marker 
+		marker = L.marker([address.lat, address.lon]).addTo(map);
+
+		//Sets the view of the map
+		map.setView([address.lat, address.lon], 10);
+      },
+
+
+      //for notify when new suggestions have been obtained for the entered text
+      suggestionsCallback: (suggestions) => {
+        console.log(suggestions);
+      }
+});
+map.addControl(addressSearchControl);
 
